3a51a80640c24aa0578fdab3921407eab71527f5
Plaifah <praypar5689@gmail.com>
2023-11-21 10:52:57 +0700

    first commit

---
+++ src/components/Compute.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+<p>
+    Full Name:{{ firstName() }}
+</p>
+    <label for="">firstName</label>
+    <input type="text" v-model="firstName" > 
+<p>
+    <label for="LastName"></label>
+    <input type="text" v-model="lastname" >
+</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ComputeView"
+        data(){
+            return{
+                firstName: Mark
+                LastName: Mumi
+            }
+        },
+        methods: {
+            fullname() {
+                
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
